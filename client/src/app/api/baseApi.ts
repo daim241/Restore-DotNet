1aa463d6c675794513012d63d0e4aa63dec592af
daim241 <daimshafi11@gmail.com>
2026-02-12 11:30:33 +0500

    - Error Handling and error button added in about page.

--- client/src/app/api/baseApi.ts
+++ client/src/app/api/baseApi.ts
@@ -1,9 +1,13 @@
 import { fetchBaseQuery, type BaseQueryApi, type FetchArgs } from "@reduxjs/toolkit/query";
 import { startLoading, stopLoading } from "../layout/uiSlice";
+import { toast } from "react-toastify";
+import { router } from "../routes/Routes";
 
 const customBaseQuery = fetchBaseQuery({
     baseUrl: 'https://localhost:5001/api'
 });
+
+type ErrorResponse = | string | { title: string } | { errors: string[] }
 
 const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
 
@@ -14,8 +18,39 @@ export const baseQueryWithErrorHandling = async (args: string | FetchArgs,
     const result = await customBaseQuery(args, api, extraOptions);
     api.dispatch(stopLoading());
     if (result.error) {
-        const {status, data} = result.error;
-        console.log({status, data});
+
+        const { status, data } = result.error;
+        const responseData = data as ErrorResponse;
+
+        console.log(result.error);
+
+        switch (status) {
+            case 400:
+                if (typeof responseData === 'string') toast.error(responseData);
+                else if ('errors' in responseData) {
+                    throw Object.values(responseData.errors).flat().join(', ')
+                }
+                else toast.error(responseData.title);
+                break;
+
+            case 401:
+                if (typeof responseData === 'object' && 'title' in responseData) {
+                    toast.error(responseData.title);
+                }
+                break;
+            
+            case 404:
+                if (typeof responseData === 'object' && 'title' in responseData)
+                    router.navigate('/not-found')
+                break;
+
+            case 500:
+                if (typeof responseData === 'object')
+                    router.navigate('/server-error', {state: {error: responseData}})
+                break;
+            default:
+                break;
+        }
     }
 
     return result;
